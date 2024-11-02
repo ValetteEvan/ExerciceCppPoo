04152833f671d92c08d54009cc10f597960b7dc9
Valette Evan <113213514+ValetteEvan@users.noreply.github.com>
2024-11-02 23:59:45 +0100

    add

--- Rpg_Textuel/Polymorphisme/Figure.hpp
+++ Rpg_Textuel/Polymorphisme/Figure.hpp
@@ -1,1 +1,53 @@
 #include <iostream>
+#include <string>
+
+class Figure
+{
+public: 
+	Figure(); 
+	virtual void afficher()const; 
+	virtual void perimetre() = 0; 
+	virtual void aire() = 0; 
+};
+
+class Triangle : public Figure
+{
+	int m_base; 
+	int m_hauteur; 
+public: 
+	Triangle(int base,int hauteur); 
+	void afficher()const;
+	void perimetre();
+	void aire();
+};
+
+class Carre : public Figure
+{
+	int m_longueur; 
+public:
+	Carre(int longueur);  
+	void afficher()const;
+	void perimetre();
+	void aire();
+};
+
+class Rectangle : public Figure
+{
+	int m_longueur; 
+	int m_largeur; 
+public:
+	Rectangle(int longueur,int largeur); 
+	void afficher()const;
+	void perimetre();
+	void aire();
+};
+
+class Cercle : public Figure 
+{
+	int m_rayon;
+public:
+	Cercle(int rayon); 
+	void afficher()const;
+	void perimetre();
+	void aire();
+};
--- Rpg_Textuel/Polymorphisme/main.cpp
+++ Rpg_Textuel/Polymorphisme/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <string>
-#include "polymorphe.h"
 #include <vector>
+#include "Figure.hpp"
 
 int main()
 {
 	
+	std::vector<Figure*> listeFigure; 
+	listeFigure.push_back(new Carre(23));
+	listeFigure.push_back(new Rectangle(23,12));
+	listeFigure.push_back(new Cercle(35));
+	listeFigure.push_back(new Triangle(23,45));
 	
+	for (int i = 0; i < listeFigure.size(); i++)
+	{
+		std::cout << "----------------\n"; 
+		listeFigure[i]->afficher(); 
+		listeFigure[i]->perimetre(); 
+		listeFigure[i]->aire(); 
+		std::cout << "----------------\n";
+	}
+
 }
