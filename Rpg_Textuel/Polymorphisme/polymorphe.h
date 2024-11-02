03616440a4482f85d422b0f99593604be5ab61e4
Valette Evan <113213514+ValetteEvan@users.noreply.github.com>
2024-11-02 23:29:24 +0100

    add

--- Rpg_Textuel/Polymorphisme/polymorphe.h
+++ Rpg_Textuel/Polymorphisme/polymorphe.h
@@ -2,20 +2,3 @@
 #include <iostream>
 #include <string>
 
-class Animal
-{
-public: 
-    virtual void makeSound() { std::cout << "Some sound\n"; }
-    virtual ~Animal() {}
-};
-
-class Cat :public Animal
-{
-public:
-    void makeSound() override { std::cout << "Meow\n"; }
-};
-
-class Dog : public Animal 
-{
-    void makeSound() override { std::cout << "Woof\n"; } 
-};
