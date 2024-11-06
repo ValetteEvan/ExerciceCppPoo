6be7f0c2a5290c616db19266f47fc9a4878cb8b5
Valette Evan <113213514+ValetteEvan@users.noreply.github.com>
2024-11-06 21:04:01 +0100

    add

--- Rpg_Textuel/Craft/Items.cpp
+++ Rpg_Textuel/Craft/Items.cpp
@@ -1,1 +1,26 @@
 #include "Items.hpp"
+
+void Items::loadItemsFromFile(std::string& _filename, std::vector<std::string>& data)
+{
+	std::fstream file(_filename);
+	std::string tmpName;
+	int tmpId;
+	int tmpQuantity;
+
+	if (!file.is_open())
+	{
+	}
+	std::cout << "Fichier ouvert\n";
+	std::string line;
+	while (std::getline(file, line))
+	{
+		std::istringstream iss(line);
+		std::string name; 
+		int id, quantity;
+		if (iss >> name >> id >> quantity)
+		{
+		
+		}
+	}
+
+}
--- Rpg_Textuel/Craft/Items.hpp
+++ Rpg_Textuel/Craft/Items.hpp
@@ -3,16 +3,18 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <sstream>
+#include <map>
 
 class Items
 {
 	std::string m_name; 
 	int m_id; 
 	int m_quantity;
-public:   
-	Items(std::string& _name,int _id,int _quantity); 
-
-	void loadItemsFromFile(std::string& _filename); 
+	std::map<std::string,int,int> container;  
+public: 
+	Items() = default; 
+	void loadItemsFromFile(std::string& _filename, std::vector<std::string>& data);
 
 	std::string getNameItems() const;
 	int getIdItems() const; 
