37c62f8b57304e7cb19bbf9936f9c33f8adbbea2
Valette Evan <113213514+ValetteEvan@users.noreply.github.com>
2024-11-12 11:11:10 +0100

    add

--- Rpg_Textuel/Buffer/buffer.cpp
+++ Rpg_Textuel/Buffer/buffer.cpp
@@ -1,23 +1,37 @@
 #include "buffer.hpp"
 
-void Buffer::addOnBuffer(int _value)
-{
-	if (buffer.size() >= MAX_BUFFER) {
-		std::cout << "Buffer plein !" << std::endl;
-	}
-	else{ buffer.push_back(_value); }
-}
+//void Buffer::addOnBuffer(int _value)
+//{
+//	if (buffer.size() >= MAX_BUFFER) {
+//		std::cout << "Buffer plein !" << std::endl;
+//	}
+//	else{ buffer.push_back(_value); }
+//}
+//
+//void Buffer::extractLastOnBuffer()
+//{
+//	if (buffer.empty()) {
+//		buffer.pop_back();
+//	}
+// }
+//
+//void Buffer::displayBuffer()
+//{
+//	for (int i = 0; i < buffer.size(); i++) {
+//		std::cout << buffer[i] << std::endl;  
+//	}
+//}
 
-void Buffer::extractLastOnBuffer()
-{
-	if (buffer.empty()) {
-		buffer.pop_back();
-	}
- }
-
-void Buffer::displayBuffer()
-{
-	for (int i = 0; i < buffer.size(); i++) {
-		std::cout << buffer[i] << std::endl;  
-	}
-}
+//void CircularBuffer::addOnBuffer(int _value)
+//{
+//	int tmpFirst; 
+//	buffer.front() = tmpFirst;  
+//	if (buffer.size() <= MAX_SIZE)
+//	{
+//		buffer.push_back(_value); 
+//	}
+//	if (buffer.size() >= MAX_SIZE)
+//	{
+//		buffer.front() = tmpFirst; 
+//	}
+//}
